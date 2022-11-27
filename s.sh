4fee3b8d0a83e6715cb3539642678b34159fe611
Stamatis Petrou <95755593+stamathsp@users.noreply.github.com>
2022-11-27 17:41:02 +0200

    Create s.sh

---
+++ s.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+echo "This is my first script. This script can make a convertion from fahrenheit degree to Celsius degree. This program was wriiten in Java"
+
+echo "program is staring..."
+
+java -cp path/to/classpath Celsius.java $@
